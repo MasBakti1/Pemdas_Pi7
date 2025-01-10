8dc4042f1b332bec4e51c1c9afd4e0a346037fb9
Ragis Rahmatulloh <77946797+icarusnp1@users.noreply.github.com>
2025-01-11 02:35:02 +0700

    perbaikan hari

--- currentdate.js
+++ currentdate.js
@@ -1,5 +1,9 @@
 const today = new Date();
-const formattedDate = today.toISOString().slice(0, 10); // Format: YYYY-MM-DD
+const formattedDate = today.toLocaleDateString().slice(0, 10); // Format: YYYY-MM-DD
 
-// Menampilkan tanggal di elemen dengan id 'current_date'
-document.getElementById('current_date').textContent = formattedDate;
+// Mendapatkan hari dalam minggu
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayName = daysOfWeek[today.getDay()]; // Mengambil hari dalam minggu (0 = Sunday, 1 = Monday, ...)
+
+// Menampilkan tanggal dan hari di elemen dengan id 'current_date'
+document.getElementById('current_date').textContent = `${formattedDate} (${dayName})`;
